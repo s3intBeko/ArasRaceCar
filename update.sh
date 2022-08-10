294834248d137c19238297eb7c95713f5a1adba5
berkay <berkayaksoy@gmail.com>
2022-08-10 08:05:18 +0300

    Edit Update

--- update.sh
+++ update.sh
@@ -3,12 +3,6 @@
 echo "\033[1;34mThere is new update\n\033[1;m"
 echo "\033[1;33mgit updating...\033[1;m"
 
-if [ $1 ]
-then
-    kill -9 $1
-else
-    killall -9 python
-fi
 
 git fetch origin master && git checkout -f origin/master
 
